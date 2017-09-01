7c250f2ad8edb4831c5e707cda076859b494af3c
Mateusz Konieczny <matkoniecz@gmail.com>
2017-09-01 22:19:34 +0200

    update watchlist

--- watchlist.rb
+++ watchlist.rb
@@ -1,3 +1,6 @@
+puts "run overpass queeries mentioned here before running watchlist itself"
+return
+
 # frozen_string_literal: true
 require 'json'
 
@@ -11,35 +14,11 @@ def watchlist_entries
   watchlist += watch_lifecycle_state_in_the_name
   #watchlist += watch_nonmilitary_military_danger
 
-  # TODO: bardzo stare highway=construction
 
   distance_in_km = 6
   watchlist << { list: get_list({ 'bicycle' => 'official' }, 50, 20, distance_in_km), message: "bicycle=official within #{distance_in_km}km from [50, 20]" }
   distance_in_km *= 5
   watchlist << { list: get_list({ 'bicycle' => 'official' }, 50, 20, distance_in_km), message: "bicycle=official within #{distance_in_km}km from [50, 20]" }
-
-  # wszystkie nawiasy; http://overpass-turbo.eu/s/gVo
-
-  # watchlist for rare landuse values in Kraków, ; w surface
-
-  # type=site relation http://overpass-turbo.eu/s/fwU
-
-#TODO:
-#puts "access=public eliminator http://overpass-turbo.eu/s/gTt"
-# more https://github.com/osmlab/name-suggestion-index/blob/master/filter.json https://lists.openstreetmap.org/pipermail/talk/2015-May/072923.html
-#should be highway=path http://overpass-turbo.eu/s/khG 
-#bicycle_road=yes w Krakowie http://overpass-turbo.eu/s/pcO
-#site=parking elimination
-#(planowany) w osm, (w budowie) nie jest lepsze
-#building=bridge w Krakowie - http://overpass-turbo.eu/s/dGR 
-#type=site relation http://overpass-turbo.eu/s/fwU 
-#name=Ogródki działkowe http://overpass-turbo.eu/s/dr3 
-#planowan* - tagowanie pod render http://overpass-turbo.eu/s/dtf 
-#landuse=basin bez natural=water
-
-#https://www.openstreetmap.org/way/33711547
-#note=taśmociąg na filarach + highway=service
-#to man_made=goods_conveyor
 
   return watchlist
 end
@@ -64,7 +43,7 @@ def currently_present_note_at(lat, lon)
   empty = '<?xml version="1.0" encoding="UTF-8"?>
 <osm version="0.6" generator="OpenStreetMap server">
 </osm>'
-  range = 0.005
+  range = 0.025
   notes = CartoCSSHelper::NotesDownloader.run_note_query(lat, lon, range).strip
   return true if notes != empty
   timestamp_in_h = (Time.now - CartoCSSHelper::NotesDownloader.cache_timestamp(lat, lon, range)).to_i / 60 / 60
@@ -182,6 +161,7 @@ def is_location_undefined(lat, lon, entry)
   end
   return false
 end
+
 def not_fully_matching_tag_set(json_response_tags, tags_in_dict)
   return true if json_response_tags.nil?
   tags_in_dict.each do |tag|
@@ -219,7 +199,7 @@ def list_of_objects_with_this_name_part(name_part)
   return get_list_from_arbitrary_query(name_part_query)
 end
 
-def suspicious_name_watchlist_entry(name, language_code_of_name, description = nil, matching_tag_list = [{}])
+def suspicious_name_watchlist_entry(name, language_code_of_name, description = nil, matching_tag_list = [{}], overpass_url = nil)
   # TODO rename to objects_using_this_name
   # TODO switch description and matching_tag_list
   # TODO add prefix to description with correct name tag value and name tag
@@ -245,14 +225,14 @@ def watch_descriptive_names
   #see also https://github.com/osmlab/name-suggestion-index - names should end here to discourage iD users from using them
   watchlist = []
   watchlist += objects_using_this_name_part('boisko do gry w', 'name used instead sport tags')
-  watchlist += suspicious_name_watchlist_entry('boisko', 'pl')
+  watchlist += suspicious_name_watchlist_entry('boisko', 'pl', 'name=boisko', [{'leisure' => 'pitch'}])
   watchlist += suspicious_name_watchlist_entry('Maszt telekomunikacyjny', 'pl')
   watchlist += suspicious_name_watchlist_entry('wiatrak', 'pl')
-  playground_complaint = 'Czy tag name nie jest tu przypadkiem błędnie użyty jako opis obiektu? leisure=playground wystarcza by oznaczyć to jako plac zabaw... Overpass query: http://overpass-turbo.eu/s/qZ6'
-  watchlist += suspicious_name_watchlist_entry('plac zabaw', 'pl', playground_complaint, [{'leisure' => 'playground'}])
-  parking_complaint = 'Is it really parking named parking or is it just a parking and name tag is incorrectly used as a description? Overpass query: http://overpass-turbo.eu/s/qZ8'
-  watchlist += suspicious_name_watchlist_entry('playground', 'en', [{'leisure' => 'playground'}])
-  watchlist += suspicious_name_watchlist_entry('parking', 'pl', parking_complaint, [{'amenity' => 'parking'}])
+  playground_complaint = 'Czy tag name nie jest tu przypadkiem błędnie użyty jako opis obiektu? leisure=playground wystarcza by oznaczyć to jako plac zabaw.'
+  watchlist += suspicious_name_watchlist_entry('plac zabaw', 'pl', playground_complaint, [{'leisure' => 'playground'}], 'http://overpass-turbo.eu/s/qZ6')
+  parking_complaint = 'Is it really parking named parking or is it just a parking and name tag is incorrectly used as a description?'
+  watchlist += suspicious_name_watchlist_entry('playground', 'en', [{'leisure' => 'playground'}], 'http://overpass-turbo.eu/s/qZ8')
+  #watchlist += suspicious_name_watchlist_entry('parking', 'pl', parking_complaint, [{'amenity' => 'parking'}])
   watchlist += suspicious_name_watchlist_entry('parking bezpłatny', 'pl', parking_complaint, [{'amenity' => 'parking'}])
   watchlist += suspicious_name_watchlist_entry('parking strzeżony', 'pl', parking_complaint, [{'amenity' => 'parking'}])
   watchlist += suspicious_name_watchlist_entry('Parking Strzeżony', 'pl', parking_complaint, [{'amenity' => 'parking'}])
@@ -262,10 +242,11 @@ def watch_descriptive_names
   watchlist += suspicious_name_watchlist_entry('dojazd do przedszkola', 'pl')
   # dojazd do
   #watchlist += suspicious_name_watchlist_entry('apteka', 'pl', 'name=apteka - overpass query: http://overpass-turbo.eu/s/qZa') - zakomentowane bo styl domyślny ma słabą ikonkę
-  watchlist += suspicious_name_watchlist_entry('kiosk', 'pl', 'query to detect this problem in obvious form - http://overpass-turbo.eu/s/qYg (in Poland, for shop=kiosk)')
+  watchlist += suspicious_name_watchlist_entry('kiosk', 'pl', 'shop=kiosk', [{'shop' => 'kiosk'}], 'http://overpass-turbo.eu/s/qYg')
   # watchlist += suspicious_name_watchlist_entry('warzywniak')
   # watchlist += suspicious_name_watchlist_entry('Warzywniak')
-  watchlist += suspicious_name_watchlist_entry('śmietnik', 'pl', 'for areas OSM data sometimes makes clea that <amenity = waste_disposal> is missing http://overpass-turbo.eu/s/qZh')
+  complaint = 'for areas OSM data sometimes makes clear that <amenity = waste_disposal> is missing http://overpass-turbo.eu/s/qZh'
+  watchlist += suspicious_name_watchlist_entry('śmietnik', 'pl', complaint, [{'amenity' => 'waste_disposal'}])
   watchlist += suspicious_name_watchlist_entry('drzewo', 'pl')
   watchlist += suspicious_name_watchlist_entry('kamieniołom', 'pl')
   watchlist += suspicious_name_watchlist_entry('quarry', 'en')
@@ -276,7 +257,7 @@ def watch_descriptive_names
   watchlist += suspicious_name_watchlist_entry('fontanna', 'pl')
   watchlist += suspicious_name_watchlist_entry('fountain', 'en')
   watchlist += suspicious_name_watchlist_entry('las', 'pl', 'name=las', [{'natural' => 'wood'}, {'landuse' => 'forest'}])
-  watchlist += suspicious_name_watchlist_entry('forest', 'en', 'name=forest query to detect - http://overpass-turbo.eu/s/qYi ', [{'natural' => 'wood'}, {'landuse' => 'forest'}])
+  watchlist += suspicious_name_watchlist_entry('forest', 'en', 'name=forest', [{'natural' => 'wood'}, {'landuse' => 'forest'}], 'http://overpass-turbo.eu/s/qYi')
   watchlist += suspicious_name_watchlist_entry('big forest', 'en', 'name=big forest', [{'natural' => 'wood'}, {'landuse' => 'forest'}])
   watchlist += suspicious_name_watchlist_entry('small forest', 'en', 'name=small forest', [{'natural' => 'wood'}, {'landuse' => 'forest'}])
 
@@ -284,6 +265,7 @@ def watch_descriptive_names
   watchlist += suspicious_name_watchlist_entry('wieża kościelna', 'pl', 'wieża kościelna (zamienić na   description = Wieża kościelna?, dodać man_made = tower), overpass: http://overpass-turbo.eu/s/qZo')
   watchlist += suspicious_name_watchlist_entry('mieszkanie', 'pl')
   watchlist += suspicious_name_watchlist_entry('dom', 'pl', 'name=dom, overpass: http://overpass-turbo.eu/s/qZn', [{'building' => :any_value}, {'historic' => 'ruins'}])
+  #dom - also in Czechia
   #TODO - other values
   watchlist += suspicious_name_watchlist_entry('obora', 'pl', 'name=obora', [{'building' => :any_value}])
   watchlist += suspicious_name_watchlist_entry('barn', 'en', 'name=barn', [{'building' => :any_value}])
@@ -291,12 +273,13 @@ def watch_descriptive_names
   watchlist += suspicious_name_watchlist_entry('restauracja', 'pl', 'name=restauracja', [{'amenity' => 'restaurant'}])
   watchlist += suspicious_name_watchlist_entry('restaurant', 'en', 'name=restaurant', [{'amenity' => 'restaurant'}])
   #TODO - not only for amenity=restaurant
+  watchlist += suspicious_name_watchlist_entry('sklep', 'en', 'name=sklep', [{'shop' => :any_value}])
 
   watchlist += suspicious_name_watchlist_entry('open field', 'en', 'what kind of open field is here?')
-  watchlist += suspicious_name_watchlist_entry('water tap', 'en', "water tap - to copy: <\n  man_made = water_tap\n
-  description = Water tap\n> overpass query helper: http://overpass-turbo.eu/s/qZe")
-  watchlist += suspicious_name_watchlist_entry('park', 'en', 'name=park/name=Park overpass query: http://overpass-turbo.eu/s/qZb')
-  watchlist += suspicious_name_watchlist_entry('park', 'pl', 'name=park/name=Park overpass query: http://overpass-turbo.eu/s/qZb')
+  complaint = "water tap - to copy: <\n  man_made = water_tap\n  description = Water tap\n> overpass query helper: http://overpass-turbo.eu/s/qZe"
+  watchlist += suspicious_name_watchlist_entry('water tap', 'en', complaint, [{'man_made' => 'water_tap'}])
+  watchlist += suspicious_name_watchlist_entry('park', 'en', 'name=park/name=Park', [{'leisure' => 'park'}], 'http://overpass-turbo.eu/s/qZb')
+  watchlist += suspicious_name_watchlist_entry('park', 'pl', 'name=park/name=Park', [{'leisure' => 'park'}], 'http://overpass-turbo.eu/s/qZb')
 
 =begin
  /*
@@ -346,20 +329,25 @@ end
 
 def watch_valid_tags_unexpected_in_krakow
   watchlist = []
-  range_in_km = 3
+  range_in_km = 6
   watchlist << { list: get_list({ 'horse' => 'designated' }, 50, 20, range_in_km), message: "(#{range_in_km}km range) horse=designated" }
-  range_in_km = 30
+  range_in_km = 40
   watchlist << { list: get_list({ 'horse' => 'designated' }, 50, 20, range_in_km), message: "(#{range_in_km}km range) horse=designated" }
 
-  range_in_km = 6
-  watchlist << { list: get_list({ 'highway' => 'bridleway' }, 50, 20, range_in_km), message: "(#{range_in_km}km range) highway=bridleway Czy naprawdę tu w Krakowie jest urwany kawałek szlaku dla koni?" }
   range_in_km = 9
   watchlist << { list: get_list({ 'highway' => 'bridleway' }, 50, 20, range_in_km), message: "(#{range_in_km}km range) highway=bridleway Czy naprawdę tu w Krakowie jest urwany kawałek szlaku dla koni?" }
+  range_in_km = 12
+  watchlist << { list: get_list({ 'highway' => 'bridleway' }, 50, 20, range_in_km), message: "(#{range_in_km}km range) highway=bridleway Czy naprawdę tu w Krakowie jest urwany kawałek szlaku dla koni?" }
 
-  range_in_km = 250
+  range_in_km = 600
   watchlist << { list: get_list({ 'highway' => 'bus_guideway' }, 50, 20, range_in_km), message: "(#{range_in_km}km range) highway=bus_guideway Czy tu naprawdę jest coś takiego jak opisane na http://wiki.openstreetmap.org/wiki/Tag:highway=bus%20guideway?uselang=pl ? Czy po prostu zwykła droga po której tylko autobusy mogą jeździć?" }
   range_in_km *= 2
   watchlist << { list: get_list({ 'highway' => 'bus_guideway' }, 50, 20, range_in_km), message: "(#{range_in_km}km range) highway=bus_guideway Czy tu naprawdę jest coś takiego jak opisane na http://wiki.openstreetmap.org/wiki/Tag:highway=bus%20guideway?uselang=pl ? Czy po prostu zwykła droga po której tylko autobusy mogą jeździć?" }
+
+  range_in_km = 6
+  watchlist << { list: get_list({ 'historic' => 'battlefield' }, 50, 20, range_in_km), message: "(#{range_in_km}km range) historic=battlefield" }
+  range_in_km = 40
+  watchlist << { list: get_list({ 'historic' => 'battlefield' }, 50, 20, range_in_km), message: "(#{range_in_km}km range) historic=battlefield" }
 
   #TODO: exclude volcano:status   extinct
   #range_in_km = 300
@@ -433,9 +421,56 @@ If not then using **military**=danger_area just because it renders seems to be a
 http://forum.openstreetmap.org/viewtopic.php?pid=598288#p598288 - de thread
 http://www.openstreetmap.org/note/597863#map=14/68.7102/20.4516 - case of civilian rocketry range
 https://help.openstreetmap.org/questions/50552/former-military-danger-areas
+http://www.openstreetmap.org/note/605791
 '
   watchlist << { list: get_list_from_arbitrary_query(query), message: info_message }
   return watchlist
 end
 
+# wszystkie nawiasy http://overpass-turbo.eu/s/rpH
+# watchlist for rare landuse, surfave values in Kraków
+# more https://github.com/osmlab/name-suggestion-index/blob/master/filter.json https://lists.openstreetmap.org/pipermail/talk/2015-May/072923.html
+#should be highway=path http://overpass-turbo.eu/s/khG 
+#bicycle_road=yes w Krakowie http://overpass-turbo.eu/s/pcO
+#site=parking elimination
+#(planowany) w osm, (w budowie) nie jest lepsze
+#building=bridge w Krakowie - http://overpass-turbo.eu/s/dGR 
+#type=site relation http://overpass-turbo.eu/s/fwU 
+#name=Ogródki działkowe http://overpass-turbo.eu/s/dr3 
+#planowan* - tagowanie pod render http://overpass-turbo.eu/s/dtf 
+#landuse=basin bez natural=water
+
+#https://www.openstreetmap.org/way/33711547
+#note=taśmociąg na filarach + highway=service
+#to man_made=goods_conveyor
+
+# TODO: bardzo stare highway=construction
+
 #TODO - private public toilets https://www.openstreetmap.org/node/3058828370#map=19/-16.51863/35.17363&layers=N
+
+#https://www.openstreetmap.org/changeset/49785062#map=8/46.945/18.215
+#building=proposed/destroyed
+#landuse=wood
+
+
+#complicated to fix
+#http://overpass-turbo.eu/s/rmN - just tourism=attraction
+
+# Sleeping after big run (II) - next run 2017 X
+# wyczyszczone w Polsce na początku 2017 IX
+# access=public eliminator http://overpass-turbo.eu/s/rpF
+
+# Sleeping after big run (I) - next run 2018 IV
+# wyczyszczone is_in:province w jednym z województw na początku 2017 IX
+# http://overpass-turbo.eu/s/r56
+#
+#is in province http://overpass-turbo.eu/s/rkf
+#Eradicate is_in is_in:country
+#is_in:county
+#is_in:municipality
+#is_in:province
+#search for other is_in: tags on taginfo
+#(punkt widokowy) w nazwie
+
+#poprawiać za pomocą "confirm website" bazowaną na add wikidata
+#błedne linki do parafii http://overpass-turbo.eu/s/rpy
