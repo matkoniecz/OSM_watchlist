1652cee4c675ddc6e9cf5eb06af210858b87a8de
Mateusz Konieczny <matkoniecz@gmail.com>
2017-12-17 10:58:50 +0100

    split file

--- watchlist_infrastructure.rb
+++ watchlist_infrastructure.rb
@@ -1,48 +1,4 @@
-def currently_present_note_at(lat, lon)
-  # in case of note present it accepts cache
-  # in case of missing note and cache that is not current it checks to be sure
-  empty = '<?xml version="1.0" encoding="UTF-8"?>
-<osm version="0.6" generator="OpenStreetMap server">
-</osm>'
-  range = 0.025
-  if CartoCSSHelper::NotesDownloader.cache_timestamp(lat, lon, range) == nil
-    puts "note download (cache was not present) (note check of #{lat}, #{lon})"
-  end
-  notes = CartoCSSHelper::NotesDownloader.run_note_query(lat, lon, range).strip
-  return true if notes != empty
-  timestamp_in_h = (Time.now - CartoCSSHelper::NotesDownloader.cache_timestamp(lat, lon, range)).to_i / 60 / 60
-  if timestamp_in_h >= time_in_hours_that_forces_note_redoing
-    puts "note download after discarding cache (cache age was #{timestamp_in_h}h) (note check of #{lat}, #{lon})"
-    notes = CartoCSSHelper::NotesDownloader.run_note_query(lat, lon, range, invalidate_cache: true)
-  end
-  return notes != empty
-end
-
-def get_history_query(type, id)
-return   "[out:json];
-  timeline(#{type},#{id});
-  foreach(
-    retro(u(t[created]))
-    (
-      #{type}(#{id});
-      out meta;
-    );
-  );"
-end
-
-def watchlist_query(tags, lat, lon, distance)
-  query = '[timeout:250][out:json];
-(
-  ' + CartoCSSHelper::OverpassQueryGenerator.get_query_element_to_get_location(tags, lat, lon, 'node', distance) + '
-  ' + CartoCSSHelper::OverpassQueryGenerator.get_query_element_to_get_location(tags, lat, lon, 'way', distance) + '
-  ' + CartoCSSHelper::OverpassQueryGenerator.get_query_element_to_get_location(tags, lat, lon, 'relation', distance) + '
-);
-out body;
->;
-out skel qt;'
-  return query
-end
-
+require_relative 'watchlist_infrastructure_external_interfaces.rb'
 
 def count_entries(watchlist)
   count = 0
@@ -77,20 +33,6 @@ def get_list(required_tags, lat = 0, lon = 0, distance_in_km = :infinity, includ
   query = watchlist_query(required_tags, lat, lon, distance)
   list = get_list_from_arbitrary_query(query, required_tags, include_history_of_tags)
   return list
-end
-
-def get_data_from_overpass(query, explanation)
-  debug = false
-  json_string = CartoCSSHelper::OverpassQueryGenerator.get_overpass_query_results(query, explanation, debug)
-  timestamp_in_h = (Time.now - CartoCSSHelper::OverpassDownloader.cache_timestamp(query)).to_i / 60 / 60
-
-  if time_in_hours_that_forces_query_redoing >= timestamp_in_h
-    puts "not redoing #{timestamp_in_h}h old query"
-    return json_string
-  end
-
-  puts "redoing #{timestamp_in_h}h old query"
-  return CartoCSSHelper::OverpassQueryGenerator.get_overpass_query_results(query, explanation, debug, invalidate_cache: true)
 end
 
 def build_string_describing_tag_appearance_from_json_history(json_history, required_tags)
@@ -222,17 +164,4 @@ def is_failing_negative_tag_match(json_response_tags, key, value)
       # was supposed to have a different value
       return true if json_response_tags[key] == value
     end
-end
-def list_of_objects_with_this_name_part(name_part)
-  watchlist = []
-  name_part_query = '[out:json][timeout:250];
-  (
-    node["name"~"' + name_part + '"];
-    way["name"~"' + name_part + '"];
-    relation["name"~"' + name_part + '"];
-  );
-  out body;
-  >;
-  out skel qt;'
-  return get_list_from_arbitrary_query(name_part_query)
 end
---
+++ watchlist_infrastructure_external_interfaces.rb
@@ -0,0 +1,72 @@
+def get_data_from_overpass(query, explanation)
+  debug = false
+  json_string = CartoCSSHelper::OverpassQueryGenerator.get_overpass_query_results(query, explanation, debug)
+  timestamp_in_h = (Time.now - CartoCSSHelper::OverpassDownloader.cache_timestamp(query)).to_i / 60 / 60
+
+  if time_in_hours_that_forces_query_redoing >= timestamp_in_h
+    puts "not redoing #{timestamp_in_h}h old query"
+    return json_string
+  end
+
+  puts "redoing #{timestamp_in_h}h old query"
+  return CartoCSSHelper::OverpassQueryGenerator.get_overpass_query_results(query, explanation, debug, invalidate_cache: true)
+end
+
+def currently_present_note_at(lat, lon)
+  # in case of note present it accepts cache
+  # in case of missing note and cache that is not current it checks to be sure
+  empty = '<?xml version="1.0" encoding="UTF-8"?>
+<osm version="0.6" generator="OpenStreetMap server">
+</osm>'
+  range = 0.025
+  if CartoCSSHelper::NotesDownloader.cache_timestamp(lat, lon, range) == nil
+    puts "note download (cache was not present) (note check of #{lat}, #{lon})"
+  end
+  notes = CartoCSSHelper::NotesDownloader.run_note_query(lat, lon, range).strip
+  return true if notes != empty
+  timestamp_in_h = (Time.now - CartoCSSHelper::NotesDownloader.cache_timestamp(lat, lon, range)).to_i / 60 / 60
+  if timestamp_in_h >= time_in_hours_that_forces_note_redoing
+    puts "note download after discarding cache (cache age was #{timestamp_in_h}h) (note check of #{lat}, #{lon})"
+    notes = CartoCSSHelper::NotesDownloader.run_note_query(lat, lon, range, invalidate_cache: true)
+  end
+  return notes != empty
+end
+
+def get_history_query(type, id)
+return   "[out:json];
+  timeline(#{type},#{id});
+  foreach(
+    retro(u(t[created]))
+    (
+      #{type}(#{id});
+      out meta;
+    );
+  );"
+end
+
+def watchlist_query(tags, lat, lon, distance)
+  query = '[timeout:250][out:json];
+(
+  ' + CartoCSSHelper::OverpassQueryGenerator.get_query_element_to_get_location(tags, lat, lon, 'node', distance) + '
+  ' + CartoCSSHelper::OverpassQueryGenerator.get_query_element_to_get_location(tags, lat, lon, 'way', distance) + '
+  ' + CartoCSSHelper::OverpassQueryGenerator.get_query_element_to_get_location(tags, lat, lon, 'relation', distance) + '
+);
+out body;
+>;
+out skel qt;'
+  return query
+end
+
+def list_of_objects_with_this_name_part(name_part)
+  watchlist = []
+  name_part_query = '[out:json][timeout:250];
+  (
+    node["name"~"' + name_part + '"];
+    way["name"~"' + name_part + '"];
+    relation["name"~"' + name_part + '"];
+  );
+  out body;
+  >;
+  out skel qt;'
+  return get_list_from_arbitrary_query(name_part_query)
+end
